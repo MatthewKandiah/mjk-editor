040ea8b52ae847facd23152a03d2985956764227
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-02-02 09:32:26 +0000

    Neater fatal error reporting

--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,30 @@ const c = @cImport({
     @cInclude("SDL2/SDL_ttf.h");
 });
 
+pub fn fatal(comptime fmt: []const u8, args: anytype) noreturn {
+    std.debug.print(fmt, args);
+    std.debug.print("\n", .{});
+    std.process.exit(1);
+}
+
+pub fn assert(condition: bool, comptime fmt: []const u8, args: anytype) void {
+    if (!condition) {
+        fatal(fmt, args);
+    }
+}
+
 pub fn main() !void {
     const sdl_init = c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_EVENTS);
-    std.debug.assert(sdl_init == 0);
+    assert(sdl_init == 0, "ERROR - SDL_Init failed: {}", .{sdl_init});
 
     const ttf_init = c.TTF_Init();
-    std.debug.assert(ttf_init == 0);
+    assert(ttf_init == 0, "ERROR - TTF_Init failed: {}", .{ttf_init});
 
-    const jetbrains_mono_font = c.TTF_OpenFont("./font/jetbrains-mono/JetBrainsMono-Regular.ttf", 24) orelse std.debug.panic("Failed to load font\n", .{});
+    const jetbrains_mono_font = c.TTF_OpenFont("./font/jetbrains-mono/JetBrainsMono-Regular.ttf", 24) orelse fatal("ERROR - Loading JetBrainsMono font failed", .{});
     defer c.TTF_CloseFont(jetbrains_mono_font);
 
     const colour = c.SDL_Color{ .r = 0, .g = 255, .b = 0, .a = 0 };
     const text_surface = c.TTF_RenderText_Solid(jetbrains_mono_font, "Hello TTF", colour);
-    std.debug.print("text_surface: {}\n", .{text_surface.*});
 
     const window = c.SDL_CreateWindow(
         "mjk",
@@ -26,7 +37,7 @@ pub fn main() !void {
         800,
         600,
         c.SDL_WINDOW_RESIZABLE,
-    ) orelse std.debug.panic("Failed to create window", .{});
+    ) orelse fatal("ERROR - SDL_CreateWindow failed", .{});
     var window_surface = c.SDL_GetWindowSurface(window);
 
     var event: c.SDL_Event = undefined;
@@ -58,10 +69,9 @@ pub fn main() !void {
             window_surface,
             @ptrCast(&dst_rect),
         );
-        std.debug.assert(blit_res == 0);
+        assert(blit_res == 0, "ERROR - SDL_BlibSurface failed: {}", .{blit_res});
 
         const update_res = c.SDL_UpdateWindowSurface(window);
-        std.debug.print("update_res: {}\n", .{update_res});
-        std.debug.assert(update_res == 0);
+        assert(update_res == 0, "ERROR - SDL_UpdateWindowSurface failed: {}", .{update_res});
     }
 }
