59743e0386d44c50bd5aab221899c05210f237f6
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-02-11 22:28:29 +0000

    quick check that we can handle non-fixed-width fonts too

--- src/main.zig
+++ src/main.zig
@@ -16,21 +16,24 @@ pub fn main() !void {
     const out_filepath = "debug/test.out.txt";
     const buffer = try pf.readFile(allocator, in_filepath);
 
-    const font_filepath = "font/ubuntu-mono/ubuntu_mono.ttf";
+    // const font_filepath = "font/ubuntu-mono/ubuntu_mono.ttf";
+    const font_filepath = "font/roboto/roboto-regular.ttf";
     const font_size = 24;
     const font = try Font.init(allocator, font_filepath, font_size);
 
-    const char_a = font.table.get('a') orelse std.debug.panic("blew up\n", .{});
-    for (0..char_a.height) |j| {
-        for (0..char_a.width) |i| {
-            const index = j * char_a.width + i;
-            if (char_a.data[index]) {
-                std.debug.print("#", .{});
-            } else {
-                std.debug.print(".", .{});
+    for ('a'..'a' + 26) |char| {
+        const char_a = font.table.get(@intCast(char)) orelse std.debug.panic("blew up\n", .{});
+        for (0..char_a.height) |j| {
+            for (0..char_a.width) |i| {
+                const index = j * char_a.width + i;
+                if (char_a.data[index]) {
+                    std.debug.print("#", .{});
+                } else {
+                    std.debug.print(".", .{});
+                }
             }
+            std.debug.print("\n", .{});
         }
-        std.debug.print("\n", .{});
     }
 
     var event: c.SDL_Event = undefined;
