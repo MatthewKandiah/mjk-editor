bc901b1be667643f274bd3e8b761c187a542f0a3
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-03-02 17:25:12 +0000

    Boilerplate for executables to generate and check screenshots

--- build.zig
+++ build.zig
@@ -16,4 +16,30 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_ttf");
     b.installArtifact(exe);
+
+    const generate_screenshots_exe = b.addExecutable(.{
+        .name = "generate-screenshots",
+        .root_source_file = b.path("src/test/generateScreenshots.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    generate_screenshots_exe.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "vendor" } });
+    generate_screenshots_exe.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "vendor/stb_impl.c" } } });
+    generate_screenshots_exe.linkLibC();
+    generate_screenshots_exe.linkSystemLibrary("SDL2");
+    generate_screenshots_exe.linkSystemLibrary("SDL2_ttf");
+    b.installArtifact(generate_screenshots_exe);
+
+    const check_screenshots_exe = b.addExecutable(.{
+        .name = "check-screenshots",
+        .root_source_file = b.path("src/test/checkScreenshots.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    check_screenshots_exe.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "vendor" } });
+    check_screenshots_exe.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "vendor/stb_impl.c" } } });
+    check_screenshots_exe.linkLibC();
+    check_screenshots_exe.linkSystemLibrary("SDL2");
+    check_screenshots_exe.linkSystemLibrary("SDL2_ttf");
+    b.installArtifact(check_screenshots_exe);
 }
