1fdf0091a0f8c2a48791e2381cc050a359e24888
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-03-29 14:25:02 +0000

    note

--- src/lib/buffer.zig
+++ src/lib/buffer.zig
@@ -87,6 +87,9 @@ pub const Buffer = struct {
         return true;
     }
 
+    // TODO-Matt: SDL Keycodes for printable characters are represented by their Unicode code points
+    // think we can use that fact to avoid writing a massive switch statement!
+    // https://wiki.libsdl.org/SDL2/SDLKeycodeLookup
     fn handleKeyDownInsert(self: *Self, key: i32, is_shift_held: bool, is_caps_lock_active: bool) !bool {
         switch (key) {
             c.SDLK_ESCAPE => self.switchToNormal(),
